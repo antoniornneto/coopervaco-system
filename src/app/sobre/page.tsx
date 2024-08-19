eb44fdeb28edf83f6bb3aced2bbdf73e902fdf64
antoniornneto <antonionascneto@gmail.com>
2024-08-19 11:33:05 -0300

    finish and adding about page

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -1,5 +1,9 @@
 import Header from "@/components/ui/header";
 import BgImage from "@/components/ui/imageBG";
+import Image from "next/image";
+import financialImg from "../../../public/assets/financeiro.png";
+import coordenation from "../../../public/assets/coordenacao.png";
+import Footer from "@/components/ui/footer";
 
 export default function SobrePage() {
   return (
@@ -7,9 +11,9 @@ export default function SobrePage() {
       <title>Coopervaço - Sobre</title>
       <Header />
       <BgImage />
-      <div className="flex flex-col w-screen items-center">
-        {/* About */}
-        <div className="w-[60%] flex gap-2 justify-center py-10">
+      <div className="flex flex-col w-screen h-screen justify-center items-center">
+        {/* About section */}
+        <div className="w-[60%] flex gap-4 justify-center py-10 flex-wrap md:flex-col md:[90%]">
           <div className="flex-1 space-y-4">
             <hr className="border-[#6eaa64] border-[1px] w-14" />
             <h1 className="text-4xl">Sobre a Coopervaço</h1>
@@ -34,7 +38,49 @@ export default function SobrePage() {
             </p>
           </div>
         </div>
+        {/* Activities section */}
+        <div className="w-[60%] flex flex-col justify-center py-10 gap-14 md:[90%]">
+          <div className="flex flex-col items-center gap-3">
+            <h1 className="uppercase font-bold">Atividades</h1>
+            <hr className="w-10 border-[1px]" />
+          </div>
+          <div className="flex items-center gap-20 md:flex-col">
+            <Image
+              src={financialImg}
+              width={0}
+              height={0}
+              alt="Ícone de uma lâmpada com um cifrão dentro com fundo verde"
+              className="w-24 h-24"
+            />
+            <div className="flex-1 space-y-4">
+              <h3 className="font-bold">EDUCAÇÃO FINANCEIRA</h3>
+              <p>
+                A cooperativa, com base na colaboração recíproca a que se propõe
+                seus cooperados, tem por objeto social a atividades em educação
+                financeira.
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center gap-20 md:flex-col">
+            <Image
+              src={coordenation}
+              width={0}
+              height={0}
+              alt="Ícone de uma lâmpada com um cifrão dentro com fundo verde"
+              className="w-24 h-24"
+            />
+            <div className="flex-1 space-y-4">
+              <h3 className="font-bold">COORDENAÇÃO </h3>
+              <p>
+                Realizar trabalhos de voltados para Coordenação de certificações
+                bancarias e Educação Financeiras da categoria no vale do aço e
+                adjacências.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
+      <Footer />
     </main>
   );
 }
