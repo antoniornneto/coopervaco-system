2b060df92461a051718dcad744f94ada516f92ef
Antonio Nasicmento <antonionascneto@gmail.com>
2025-04-01 11:13:36 -0300

    fix: teste

--- src/app/api/get-all-employees/route.ts
+++ src/app/api/get-all-employees/route.ts
@@ -1,12 +1,11 @@
-export const revalidate = 0; // Garante que a API sempre busca dados novos
+export const dynamic = "force-dynamic"; // Desativa cache automático do Next.js API
 
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
   const employees = await db.employee.findMany();
-
   return NextResponse.json(employees, {
-    headers: { "Cache-Control": "no-store" },
+    headers: { "Cache-Control": "no-store, must-revalidate" },
   });
 }
