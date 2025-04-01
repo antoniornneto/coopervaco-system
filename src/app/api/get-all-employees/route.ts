584e7f03fae5e273300fe9b580f7755b0930d9e5
Antonio Nasicmento <antonionascneto@gmail.com>
2025-04-01 10:56:53 -0300

    fix: testando desligar cache do prisma

--- src/app/api/get-all-employees/route.ts
+++ src/app/api/get-all-employees/route.ts
@@ -1,7 +1,12 @@
+export const revalidate = 0; // Garante que a API sempre busca dados novos
+
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
   const employees = await db.employee.findMany();
-  return NextResponse.json(employees);
+
+  return NextResponse.json(employees, {
+    headers: { "Cache-Control": "no-store" },
+  });
 }
